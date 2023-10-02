5559c6b0fdd976b4739b55bfc15b16e29ecdd5b5
robin-bosch <alphaclass@web.de>
2023-10-02 19:30:29 +0200

    Added FAQ section accordion functionality

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ document.querySelector("#burger-btn").addEventListener("click", function() {
         document.querySelector("#burger-btn").classList.add("open");
     }
 })
+
+
+var acc = document.getElementsByClassName("faq-question-header");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("open");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }

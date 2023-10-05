3f7e9c7546e96a728837b1948ce1843149010778
robin-bosch <alphaclass@web.de>
2023-10-05 21:28:17 +0200

    Code cleanup

--- script.js
+++ script.js
@@ -1,75 +1,83 @@
-document.querySelector("#burger-btn").addEventListener("click", toggleMenu);
-document.querySelector("#close-btn").addEventListener("click", toggleMenu);
-
-
+/**
+ * Toggles the open and close animation for the menu
+ * #burger-btn and #header-logo get their own open class to toggle their visibility (hidden when open)
+ */
 function toggleMenu() {
   document.querySelector("#mobile-menu").classList.toggle("open");
   document.querySelector("#burger-btn").classList.toggle("open");
   document.querySelector("#header-logo").classList.toggle("open");
 
 }
+// Attached to the menu button and the close button in the menu
+document.querySelector("#burger-btn").addEventListener("click", toggleMenu);
+document.querySelector("#close-btn").addEventListener("click", toggleMenu);
 
-var acc = document.getElementsByClassName("faq-question-header");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-      this.classList.toggle("open");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-      }
+/**
+ * Scrolls the feature container to the left or right depending on the clicked tab
+ * 
+ * @param {*} tabIndex - Index of the tab to show
+ */
+function showTab(tabIndex) {
+    // Remove the active status where the status is not active for every link 
+    // Add the active status to the link where the index matches the new index
+    document.querySelectorAll('.feature-nav-link').forEach((link, index) => {
+        if(index == tabIndex) {
+            link.classList.add("active");
+        }
+        else {
+            link.classList.remove("active");
+        }
     });
-  }
+    document.querySelectorAll('.feature-tab').forEach((tab) => {
+        tab.style.transform = `translateX(-${tabIndex*100}%)`;
+    });
+}
 
-const panels = document.querySelectorAll('.feature-tab');
-const links = document.querySelectorAll('.feature-nav-link');
+//Show the first tab
+showTab(0);
 
-function showPanel(panelIndex) {
-  links.forEach((link, index) => {
-    if(index == panelIndex) {
-      link.classList.add("active");
+
+/**
+ * Attaches an event listener to every FAQ question to expand it.
+ */
+document.querySelectorAll(".faq-question-header").forEach((question, index) => {
+    question.addEventListener("click", function() {
+
+        this.classList.toggle("open");
+        let questionBody = this.nextElementSibling;
+
+        if (questionBody.style.maxHeight) {
+            questionBody.style.maxHeight = null;
+        }
+        else {
+            questionBody.style.maxHeight = questionBody.scrollHeight + "px";
+        }
+    });
+})
+
+
+/*
+* Prevent the submit to inject the own error message if the email address is not valid
+*/
+document.querySelector('#contact-form').addEventListener('submit', function (e) {
+    e.preventDefault(); 
+
+    const emailInput = document.getElementById('email');
+    const emailError = document.getElementById('email-error');
+
+
+    // Check if the email valid
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailInput.value)) {
+        emailError.style.display = 'none';
+        emailInput.classList.remove('input-error');
+
+        // Submit
+        // this.submit();
     }
     else {
-      console.log("this is not active")
-      link.classList.remove("active");
+        // Display error message
+        emailError.style.display = 'block';
+        emailInput.classList.add('input-error');
     }
 });
-  panels.forEach((panel, index) => {
-      panel.style.transform = `translateX(-${panelIndex*100}%)`;
-  });
-}
-
-
-document.getElementById('#contact-form').addEventListener('submit', function (e) {
-  console.log("submit")
-  e.preventDefault(); // Prevent the form from submitting
-
-  const emailInput = document.getElementById('email');
-  const emailError = document.getElementById('email-error');
-
-  // Check if the email is not valid (you can use a more comprehensive validation)
-  if (!isValidEmail(emailInput.value)) {
-    emailError.style.display = 'block';
-    emailInput.classList.add('input-error');
-  } else {
-    // Clear the error message and class if the email is valid
-    emailError.style.display = 'none';
-    emailInput.classList.remove('input-error');
-    
-    // Submit the form or perform other actions
-    this.submit();
-  }
-});
-
-function isValidEmail(email) {
-  // Implement your email validation logic here
-  // You can use regular expressions or other methods
-  // This is a simple example
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-// Initially show the first panel
-showPanel(0);
